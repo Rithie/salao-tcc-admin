74dddd34b7be29b9016628a75294077154b1b5e4
Roberto Barbosa <robertojf95@gmail.com>
2017-10-20 18:52:56 -0300

    Alterações

--- src/app/pages/pages.menu.ts
+++ src/app/pages/pages.menu.ts
@@ -6,7 +6,7 @@ export const PAGES_MENU = [
         path: 'dashboard',
         data: {
           menu: {
-            title: 'general.menu.dashboard',
+            title: 'Inicio',
             icon: 'ion-android-home',
             selected: false,
             expanded: false,
@@ -134,36 +134,36 @@ export const PAGES_MENU = [
           },
         ],
       },
-      {
-        path: '',
-        data: {
-          menu: {
-            title: 'general.menu.pages',
-            icon: 'ion-document',
-            selected: false,
-            expanded: false,
-            order: 650,
-          }
-        },
-        children: [
-          {
-            path: ['/login'],
-            data: {
-              menu: {
-                title: 'general.menu.login'
-              }
-            }
-          },
-          {
-            path: ['/register'],
-            data: {
-              menu: {
-                title: 'general.menu.register'
-              }
-            }
-          }
-        ]
-      },
+      // {
+      //   path: '',
+      //   data: {
+      //     menu: {
+      //       title: 'general.menu.pages',
+      //       icon: 'ion-document',
+      //       selected: false,
+      //       expanded: false,
+      //       order: 650,
+      //     }
+      //   },
+      //   children: [
+      //     {
+      //       path: ['/login'],
+      //       data: {
+      //         menu: {
+      //           title: 'general.menu.login'
+      //         }
+      //       }
+      //     },
+      //     {
+      //       path: ['/register'],
+      //       data: {
+      //         menu: {
+      //           title: 'general.menu.register'
+      //         }
+      //       }
+      //     }
+      //   ]
+      // },
     ]
   }
 ];
